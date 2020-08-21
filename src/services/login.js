74adc74c4ef75552e4efe6cb85ff9d6a49156d0c
icwater <285386663@qq.com>
2020-08-21 15:57:36 +0800

    搭建react_admin

---
+++ src/services/login.js
@@ -0,0 +1,18 @@
+import request from '@/utils/request';
+
+export async function accountLogin(params) {
+  return request('/api/auth/login', {
+    method: 'POST',
+    data: params,
+  });
+}
+//
+// export async function fakeAccountLogin(params) {
+//   return request('/api/login/account', {
+//     method: 'POST',
+//     data: params,
+//   });
+// }
+export async function getFakeCaptcha(mobile) {
+  return request(`/api/login/captcha?mobile=${mobile}`);
+}
